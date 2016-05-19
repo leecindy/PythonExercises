0e1eaa987e51d4cae1700961ad1145e9baf6f0bb
leecindy <cindy.lee122@gmail.com>
2016-05-18 20:16:26 -0700

    Calculate age in days
    updated comments

--- DaysOld.py
+++ DaysOld.py
@@ -1,11 +1,10 @@
-
 # Given your birthday and the current date, calculate your age in days. 
 # Account for leap days. 
 #
 # Assume that the birthday and current date are correct dates (and no 
 # time travel). 
 #
-
+# Determine if this is a leap year
 def isleap(year):
     if year % 4 !=0:
         return False
